7b6db0b4532519d8323ca40f8237d9bf372906ed
PAULINE-ASHLEY <ashleyondiek@gmail.com>
2023-10-05 15:50:12 +0300

    Navbar and Footer

---
+++ ash-coin-react-web/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h2 className="text-center font-bold text-4xl my-20">Welcome to Ash Coin Homepage</h2>
+    </main>
+  )
+}
--- ash-coin-react-web/src/app/page.js
+++
@@ -1,5 +0,0 @@
-// import Image from 'next/image';
-
-export default function Home() {
-  return <p className="font-bold text-4xl py-10 px-10 text-red-500">ASH COIN</p>;
-}
